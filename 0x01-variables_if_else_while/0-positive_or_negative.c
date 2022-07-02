761dba4f62f4e31eab3f18d15b85b4545500363a
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-02 16:32:40 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 *main -> assign a random number to the variable n each time it is executed 
 *and print out based condition
@@ -8,17 +9,18 @@
 */
 int main(void)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n)
-}
-return (0);
+	int n;
+	'srand' ('time'(0));
+	n = 'rand'() - 'RAND_MAX' / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+		if (n == 0)
+		{
+			printf("%d is zero\n", n);
+		}
+			if (n < 0)
+			{
+				printf("%d is negative\n", n);
+			}
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	'srand' ('time'(0));
+	n = 'rand'() - 'RAND_MAX' / 2;
 	printf("Last digit of %d is ",n);
 	if (n > 5)
 	{
