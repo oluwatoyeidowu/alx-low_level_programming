55da2735a4197538287366e62f4babe9d7db49e2
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-05 00:59:04 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,7 +10,7 @@ int main(void)
 	int ch;
 	int n;
 	int m;
-	
+
 	for (ch = 48; ch <= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
@@ -22,10 +22,10 @@ int main(void)
 					putchar(ch);
 					putchar(n);
 					putchar(m);
-					
+
 					if (ch != 55 || n != 56 || m != 57)
 					{
-                                	putchar(44);
+					putchar(44);
 					putchar(32);
 					}
 				}
