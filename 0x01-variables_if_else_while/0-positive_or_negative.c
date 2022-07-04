e6a51628ecec1571197e2063b7db0524b3bb22ed
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-04 21:04:10 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 *main -> assign a random number to the variable n each time it is executed 
 *and print out based condition
@@ -9,18 +8,20 @@
 */
 int main(void)
 {
-	int n;
-	'srand' ('time'(0));
-	n = 'rand'() - 'RAND_MAX' / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-		if (n == 0)
-		{
-			printf("%d is zero\n", n);
-		}
-			if (n < 0)
-			{
-				printf("%d is negative\n", n);
-			}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is positive\n", n);
+}
+return (0);
 }
