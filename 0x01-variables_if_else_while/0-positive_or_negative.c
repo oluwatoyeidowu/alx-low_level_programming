845ec280b7d3645ee15a439e68047b8c5283ad49
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-04 23:34:34 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main -> assign a random number to the variable n each time it is executed 
-*and print out based condition
+* main -> assign a random number to the variable n each time it is executed
+* and print out based condition
 *Return : always 0
 */
 int main(void)
