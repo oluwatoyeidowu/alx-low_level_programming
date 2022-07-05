399fbba3fce76287ec1aca5283f918c1e77fc63b
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-05 01:16:03 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*assign a random number to the variable n each time it is executed
-*print the last digit of the number stored in the variable n
-*Return : always 0
+* main - assign a random number to the variable n each time it is executed
+* print the last digit of the number stored in the variable n
+* Return: always 0
 */
 int main(void)
 {
@@ -12,6 +12,7 @@ int main(void)
 	*/
 	char ch;
 	int n;
+
 	for (n = 48; n <= 57; n++)
  	{
 		putchar(n);
