9dc6bd5beced7d0998b16950c7c68944dbecca50
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-06 02:32:22 +0100

    arithmetic operators and logical operators and relational operators

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2;
 lastn = n % 10;
 if (lastn > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lastn);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
-else if ( lastn == 0 )
+else if (lastn == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lastn);
+printf("Last digit of %d is %d and is 0\n", n, lastn);
 }
 else
 {
-printf("last digit of %d is %d and is less than 6 not 0\n", n, lastn);
+printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastn);
 }
 return (0);
 }
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,12 +20,11 @@ putchar(j % 10 + 48);
 putchar(' ');
 putchar(1 / 10 + 48);
 putchar(1 % 10 + 48);
+{
 if (j * 100 + i != 9899)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 }
