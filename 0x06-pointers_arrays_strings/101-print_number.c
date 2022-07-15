c1d1a1bd169a5194a19ddc9d4b7d647b10da50ad
Oluwatoye Idowu <oagbonjinmi@gmail.com>
2022-07-14 19:32:59 -0700

    task 9 function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,16 +4,16 @@
  * print _number - prints integer
  * @n: Integer to be printed
  */
-void print_number(int n)
+void print_number(rrr n)
 {
-	unsigned int num = n;
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-		if ((num / 10) > 0)
-		{
-			print_number(num / 10);
-			_putchar((num % 10) + '0');
-		}
+unsigned int num = n;
+
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
