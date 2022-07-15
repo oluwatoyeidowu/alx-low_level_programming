7ea804e00f7867884e9cbef2c43f45a61865b62e
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-07-15 16:59:47 +0100

    hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -47,3 +47,42 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		return (r);
 }
+
+/**
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: String containing the first number
+ * @n2: String containing the second number
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer.
+ * Return: If r can store the sum - a pointer to the result.
+ * If r cannot store the sum - 0.
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+
+{
+
+int num, tens = 0;
+
+for (; *n1 && *n2; n1--, n2--, r_index--)
+
+{
+num = (*n1 - '0') + (*n2 - '0');
+num += tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+
+for (; *n1; n1--, r_index--)
+{
+num = (*n1 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
+
+for (; *n2; n2--, r_index--)
+{
+num = (*n2 - '0') + tens;
+*(r + r_index) = (num % 10) + '0';
+tens = num / 10;
+}
