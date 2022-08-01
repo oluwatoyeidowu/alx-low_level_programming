6e3607e5f1ca013c5960ac140ebf5a7bbf496dce
Oluwatoye Idowu <oagbonjinmi@gmail.com>
2022-08-01 09:48:27 -0700

    new type struct dog idn elements task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define _DOG_H_
 
 /**
- * dog_a - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  *
  */
-typedef struct dog dog_a;
+typedef struct dog dog_t;
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -26,7 +26,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_a *new_dog(char *name, float age, char *owner);
-void free_dog(dog_a *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
