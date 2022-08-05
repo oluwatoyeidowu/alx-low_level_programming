ad8612a77296eed3f00cf53d2703df28d3d046d8
Oluwatoye Idowu <oagbonjinmi@gmail.com>
2022-08-04 19:07:30 -0700

    Variadic functions are functions which may take a variable number of arguments and are declared with an ellipsis in place of the last parameter

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
