ce6fba4ea87e0371f1495d18bec26509caaa99d1
Oluwatoye Idowu <oagbonjinmi@gmail.com>
2022-08-04 20:09:30 -0700

    Prototypes of all functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,20 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
